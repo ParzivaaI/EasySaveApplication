e177e7bb2a57dc92a8eee2380c7e1e6155a6ef39
Valentin LAURENT <0valentinlaurent@gmail.com>
2022-12-12 13:13:30 +0100

    Revert "Merge branch 'main' of https://github.com/ParzivaaI/EasySaveApplication"
    This reverts commit 0d50c4a0576e3a3ec0a2a9f7cfc2a4631d6d2b1c, reversing
    changes made to 81a88b44a7d253a6d9429ca56db7139a593c8e54.

--- WPFDEMO/WPFDEMO/ViewModel/ViewModel.cs
+++ WPFDEMO/WPFDEMO/ViewModel/ViewModel.cs
@@ -74,7 +74,7 @@ namespace WPFDEMO.ViewModel
         }
         private void AddEnglish()
         {
-            CurrentLangues = new Langues("Add a new save name :", "Add the folder location :", "Add the folder destination :", "Complete Save", "Differential Save");
+            CurrentLangues = new Langues("Add a new save name :", "Add the location of the folder :", "Add the destination of the folder :", "Complete Save", "Differential Save");
         }
         private void Addfrench()
         {
