78187f5fe7babad67c30acda855d3a3ae6b593c4
OssamaAfri <ossama.afri@viacesi.fr>
2022-12-12 14:02:01 +0100

    Indéxé

--- WPFDEMO/WPFDEMO/ViewModel/ViewModel.cs
+++ WPFDEMO/WPFDEMO/ViewModel/ViewModel.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
-using WPFDEMO.Commands;
 using WPFDEMO.Model;
 
 namespace WPFDEMO.ViewModel
